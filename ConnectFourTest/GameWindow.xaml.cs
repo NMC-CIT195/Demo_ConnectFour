bf67d41b87caecad0d20ec172036f38777d32804
johnvelis <john@johnvelis.com>
2020-02-13 10:51:03 -0500

    Add project files.

---
+++ ConnectFourTest/GameWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ConnectFourTest
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class GameWindow : Window
+    {
+        GameWindowViewModel _gameWindowViewModel;
+
+        public GameWindow()
+        {
+            _gameWindowViewModel = new GameWindowViewModel();
+            this.DataContext = _gameWindowViewModel;
+
+            InitializeComponent();
+        }
+
+        private void GameBoard_Click(object sender, RoutedEventArgs e)
+        {
+            Button boardPositionButton = sender as Button;
+            int row = int.Parse(boardPositionButton.Tag.ToString().Substring(0, 1));
+            int column = int.Parse(boardPositionButton.Tag.ToString().Substring(1, 1));
+
+            _gameWindowViewModel.PlayerMove(row, column);
+        }
+    }
+}
---
+++ ConnectFourTest/GameWindowViewModel.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConnectFourTest
+{
+    public class GameWindowViewModel : ObservableObject
+    {
+        private enum GameState
+        {
+            PLAYER_RED,
+            PLAYER_BLUE
+        }
+
+        private const string PLAYER_RED_CHIP_COLOR = "Red";
+        private const string PLAER_BLUE_CHIP_COLOR = "Blue";
+        private const string EMPTY_BOARD_LOCATION_COLOR = "Gray";
+
+        private string _currentPlayer;
+        private GameState _currentGameState;
+        private string[][] _currentBoard;
+
+        public string[][] CurrentBoard
+        {
+            get { return _currentBoard; }
+            set
+            {
+                _currentBoard = value;
+                OnPropertyChanged(nameof(CurrentBoard));
+            }
+        }
+
+        public string CurrentPlayer
+        {
+            get { return _currentPlayer; }
+            set
+            {
+                _currentPlayer = value;
+                OnPropertyChanged(nameof(CurrentPlayer));
+            }
+        }
+
+        public GameWindowViewModel()
+        {
+            InitializeGame();
+        }
+
+        private void InitializeGame()
+        {
+            _currentGameState = GameState.PLAYER_RED;
+            CurrentPlayer = GameState.PLAYER_RED.ToString();
+
+            CurrentBoard = new string[2][];
+            CurrentBoard[0] = new string[2];
+            CurrentBoard[1] = new string[2];
+            InitializeGameboard();
+        }
+
+        /// <summary>
+        /// fill the game board array with gray pieces
+        /// </summary>
+        public void InitializeGameboard()
+        {
+            //
+            // Set all PlayerPiece array values to "None"
+            //
+            for (int row = 0; row < 2; row++)
+            {
+                for (int column = 0; column < 2; column++)
+                {
+                    CurrentBoard[row][column] = EMPTY_BOARD_LOCATION_COLOR;
+                }
+            }
+        }
+
+        internal void PlayerMove(int row, int column)
+        {
+            if (CurrentBoard[row][column] == "Gray")
+            {
+                if (_currentGameState == GameState.PLAYER_RED)
+                {
+                    CurrentBoard[row][column] = PLAYER_RED_CHIP_COLOR;
+                    OnPropertyChanged(nameof(CurrentBoard));
+                    _currentGameState = GameState.PLAYER_BLUE;
+                    CurrentPlayer = GameState.PLAYER_BLUE.ToString();
+                }
+                else
+                {
+                    CurrentBoard[row][column] = PLAER_BLUE_CHIP_COLOR;
+                    OnPropertyChanged(nameof(CurrentBoard));
+                    _currentGameState = GameState.PLAYER_RED;
+                    CurrentPlayer = GameState.PLAYER_RED.ToString();
+                }
+
+            }
+        }
+    }
+}
